d10713e4bddad997dfab4858a98dbd392958be84
Dwight Gunning <dgunning@gmail.com>
2025-07-03 15:01:04 -0400

    Initial Commit

---
+++ MathMastery/MathMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathMasteryApp.swift
+//  MathMastery
+//
+//  Created by Dwight Gunning on 2025-07-03.
+//
+
+import SwiftUI
+
+@main
+struct MathMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathMasteryTests/MathMasteryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathMasteryTests.swift
+//  MathMasteryTests
+//
+//  Created by Dwight Gunning on 2025-07-03.
+//
+
+import Testing
+@testable import MathMastery
+
+struct MathMasteryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
